612e5ac6381a1902a90cb731293b98f8c14111f5
clefersondannylo <clefersonnascimento8@gmail.com>
2023-12-16 18:20:07 -0300

    change backend url

--- src/services/api.js
+++ src/services/api.js
@@ -3,5 +3,5 @@ import axios from "axios";
 import { BACKEND_URL_DEV, BACKEND_URL_PROD } from "../config";
 
 export const api = axios.create({
-  baseURL: BACKEND_URL_DEV,
+  baseURL: BACKEND_URL_PROD,
 });
